bf9c10a4280cc3da99f6bc2810a9d3f2d6737e77
sphere <spherein@hotmail.com>
2025-01-14 23:41:27 +0530

    Version 5

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Version 4</h1>
+      <h1>Version 5</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
